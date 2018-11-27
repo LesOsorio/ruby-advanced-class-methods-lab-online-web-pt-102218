b7aac0a691f901c61e0e43bf11240e78c0e29977
Leslie Osorio <lesosorio17@gmail.com>
2018-11-27 19:47:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
 
   def self.create_by_name(name)
     song = self.new
-    song.name = (name.split(" - ") [1].chomp(".mp3"))
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     @@all << song
     song
