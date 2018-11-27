45fe2f2e2ba6ad2e933e68ccd8779aab95858dd8
Leslie Osorio <lesosorio17@gmail.com>
2018-11-27 19:28:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name
+    if self.find_by_name == nil
+      self.create_by_name
+    else
+      self.find_by_name
+    end 
   end
 end
